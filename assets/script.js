734e24f59d9e7d4732de8af0ce3db604d8794c26
Jordi <damlabulbul26@gmail.com>
2022-03-11 16:41:49 -0800

    added edits to images and added icons

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// const myBtn = document.getElementById("myBtn");
+// const myBtnTwo = document.getElementById("myBtnTwo")
+
+// myBtn.addEventListener("click", function(e) {
+//     console.log("The Button Was Clicked")
+// });
+
+
+
+// myBtnTwo.addEventListener("click", function(e) {
+//     console.log("The Button Was Clicked")
+// });
