166ee9975656b6b987b8ffecaed84942533cf685
Jordi <damlabulbul26@gmail.com>
2022-03-12 12:19:27 -0800

    made navbar buttons open accordion

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const projectBtn = document.querySelector("#project-btn");
+const contactBtn = document.querySelector("#contact-btn");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -12,3 +15,16 @@ addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// projectBtn.addEventListener("click", (e) = {
+//     var dataBsToggle = document.getElementsByTagName('collapse')
+//     var bsCollapse = new bootstrap.Collapse(myCollapse, {
+//         toggle: false
+// })
+    
+// })
+
+// contactBtn.addEventListener("click", () => {
+//     // hamburger.classList.toggle("active");
+//     // navMenu.classList.toggle("active");
+// })
