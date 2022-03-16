938f92d26adcb54e71bc7fd97bb5be1f30d1e0ad
Jordi <damlabulbul26@gmail.com>
2022-03-16 00:51:31 -0700

    added a js function and fixing mobil view

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const projectBtn = document.querySelector("#project-btn");
-const contactBtn = document.querySelector("#contact-btn");
+
 
 
 hamburger.addEventListener("click", () => {
@@ -16,15 +15,3 @@ addEventListener("click", () => {
     navMenu.classList.remove("active");
 }))
 
-// projectBtn.addEventListener("click", (e) = {
-//     var dataBsToggle = document.getElementsByTagName('collapse')
-//     var bsCollapse = new bootstrap.Collapse(myCollapse, {
-//         toggle: false
-// })
-    
-// })
-
-// contactBtn.addEventListener("click", () => {
-//     // hamburger.classList.toggle("active");
-//     // navMenu.classList.toggle("active");
-// })
